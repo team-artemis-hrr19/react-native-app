0f2b72ce2bc84c06d6beb513d10fa5368ca8eed3
heshamabdalla <shamoh702@gmail.com>
2016-10-20 13:00:47 -0700

    merg new changes

--- OTW/index.ios.js
+++ OTW/index.ios.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 var SignUp = require('./app/components/signUp.js')
 import {
@@ -9,30 +10,15 @@ import {
 } from 'react-native';
 
 export default class OTW extends Component {
-  
+
   render() {
     return (
-<<<<<<< HEAD
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-        Branch Test
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-=======
       <NavigatorIOS
         style={styles.container}
         initialRoute={{
           title: 'On The Way',
           component: SignUp
         }} />
->>>>>>> cca4dfe68a7517fde3fbe5f8ff045a3f7c1bc949
     );
   }
 }
