63676f53c80a5160aab9ba6b920525fa9ad35eae
jonathan.deng <dengjonathan@gmail.com>
2016-10-27 22:43:12 -0400

    removed index.js in app folder

---
+++ OTW/__tests__/store/reducerSpec.js
@@ -0,0 +1,10 @@
+import rootReducer from '../../app/reducers/rootReducer';
+import chai from '_testHelper';
+
+const expect = chai.expect;
+
+describe('reducer', () =>  {
+  it('should be able to update the user', () => {
+
+  });
+});
--- OTW/app/index.js
+++
@@ -1,2 +0,0 @@
-import AppContainer from './containers/AppContainer.js';
-
