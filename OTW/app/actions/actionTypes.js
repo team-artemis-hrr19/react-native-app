fbd3d5110982afc3bb41f8b07aefed55f9254f57
jonathan.deng <dengjonathan@gmail.com>
2016-10-27 22:34:44 -0400

    updated actions

--- OTW/app/actions/actionTypes.js
+++ OTW/app/actions/actionTypes.js
@@ -1,7 +1,13 @@
 // this list is to keep all actions that can be
 // dispatched to the Redux store in one place
 
+// NAVIGATION
 export const PUSH_ROUTE = 'PUSH_ROUTE';
 export const POP_ROUTE = 'POP_ROUTE';
+
+// USER AUTHENTICATION
 export const UPDATE_USER = 'UPDATE_USER';
 export const REMOVE_USER = 'REMOVE_USER';
+
+// SENDBIRD
+export const UPDATE_SENDBIRD_USERS = 'UPDATE_SENDBIRD_USERS';
