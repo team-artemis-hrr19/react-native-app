8d1f79f977a52ba386589593a0c34dd4dd2f3d6e
David Bisrat <dabisrat@gmail.com>
2016-10-30 01:51:48 -0400

    add SB utility functions for invite frineds and groupchat work in progress

--- OTW/app/utils/sendBird.js
+++ OTW/app/utils/sendBird.js
@@ -1,7 +1,8 @@
 import SendBird from 'sendbird';
-
-const appId = 'B8C20852-7D51-4294-AF13-5DD3B4FB79B2';
+import store from '../store.js';
+const appId = '784B5E97-7863-4562-B5C1-DE3D6B6491A8';
 const sb = new SendBird({appId: appId});
+
 
 export function sendBirdConnect(userEmail, username, cb) {
   console.log('line 8 of sendbird connect not connected yet', userEmail, username)
@@ -21,7 +22,6 @@ export function sendBirdConnect(userEmail, username, cb) {
 }
 
 export function sendBirdGetUsers (cb) {
-
   const query = sb.createUserListQuery();
   query.next(function(users, err) {
     if(err) {
@@ -31,3 +31,32 @@ export function sendBirdGetUsers (cb) {
     cb(users.filter(user => user.userId !== sb.currentUser.userId));
   })
 }
+
+export function sendBirdCreateGroupChat(cb) {
+  var _friendsList = []
+  var friendsListState = store.getState().sendBirdState.get('friendsList')
+  friendsListState.forEach((item) => {_friendsList.push(item)})
+  sb.GroupChannel.createChannel(_friendsList, false, function(channel, error) {
+    if (error) {
+      console.log(channel)
+      return console.error('we got a problem',error);
+
+    }       
+    cb(channel);
+    //this.updateChannel.call(this, channel); //TODO: implement channel 
+    //this._handleNavigation({'push', route: { key: 'groupChat'}});
+  });
+ }
+
+ export function sendBirdRemoveChannelHandler(){
+   sb.removeChannelHandler('MessageHandler')
+ }
+
+ export function sendBirdCreateChannelHandler(cb){
+   var ChannelHandler = new sb.ChannelHandler();
+   ChannelHandler.onMessageReceived = function(channel, message){
+    store.getState().sendBirdState.get('messageList')
+    //_messages = _messages.concat.
+   }
+ }
+
